8c26447d1f3bd664e774933d52afa6dc21aaab21
dlchambersjr <dlchambersjr@gmail.com>
2018-08-20 21:49:09 -0700

    Save Point

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ var
   imageTwoEl = document.getElementById('choice-two'),
   imageThreeEl = document.getElementById('choice-three');
 
+var currentProductsDisplayed = [];
+var previousProductsDisplayed = [];
+
 //Create the product list
 
 var productList = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
@@ -24,53 +27,54 @@ productList.forEach(function (product) {
   new Product(product);
 });
 
-// Get three unique random numbers and put into the current array
-var currentProductsDisplayed = [];
-var previousProductsDisplayed = [];
+// Get random Product numbers and place into and Array
+// There can be no duplicate items in the currentProductDisplay or the previousProductsDisplayed
 
-function getRandomProducts() {
+function getCurrentDisplay() {
 
-  var getRandomNumber = 3;
-
-  while (getRandomNumber !== 0) {
+  for (var numberOfProducts = 1; numberOfProducts <= 3; numberOfProducts++) {
     var randomProductNumber = Math.floor(allProducts.length * Math.random());
-    console.log(randomProductNumber);
+
+    while (currentProductsDisplayed.includes(randomProductNumber) | previousProductsDisplayed.includes(randomProductNumber)) {
+      randomProductNumber = Math.floor(allProducts.length * Math.random());
+    }
+
     currentProductsDisplayed.push(randomProductNumber);
-    getRandomNumber--;
+    allProducts[randomProductNumber].views++;
   }
 
-  // Assign image source files
-  var productNumber = 0;
 
-  imageOneEl.src = allProducts[currentProductsDisplayed[productNumber]].path;
-  imageOneEl.title = allProducts[currentProductsDisplayed[productNumber]].name;
-  imageOneEl.alt = allProducts[currentProductsDisplayed[productNumber]].name;
+  imageOneEl.src = allProducts[currentProductsDisplayed[0]].path;
+  imageOneEl.title = allProducts[currentProductsDisplayed[0]].name;
+  imageOneEl.alt = allProducts[currentProductsDisplayed[0]].name;
 
-  productNumber++;
 
-  imageTwoEl.src = allProducts[currentProductsDisplayed[productNumber]].path;
-  imageTwoEl.title = allProducts[currentProductsDisplayed[productNumber]].name;
-  imageTwoEl.alt = allProducts[currentProductsDisplayed[productNumber]].name;
+  imageTwoEl.src = allProducts[currentProductsDisplayed[1]].path;
+  imageTwoEl.title = allProducts[currentProductsDisplayed[1]].name;
+  imageTwoEl.alt = allProducts[currentProductsDisplayed[1]].name;
 
-  productNumber++;
 
-  imageThreeEl.src = allProducts[currentProductsDisplayed[productNumber]].path;
-  imageThreeEl.title = allProducts[currentProductsDisplayed[productNumber]].name;
-  imageThreeEl.alt = allProducts[currentProductsDisplayed[productNumber]].name;
+  imageThreeEl.src = allProducts[currentProductsDisplayed[2]].path;
+  imageThreeEl.title = allProducts[currentProductsDisplayed[2]].name;
+  imageThreeEl.alt = allProducts[currentProductsDisplayed[2]].name;
 
-  previousProductsDisplayed = currentProductsDisplayed;
+  previousProductsDisplayed.length = 0;
 
-  currentProductsDisplayed = [];
+  previousProductsDisplayed = currentProductsDisplayed.slice();
 
-  console.log(previousProductsDisplayed, currentProductsDisplayed);
+  currentProductsDisplayed.length = 0;
+}
+var testLength = 1;
+// getCurrentDisplay();
+
+while (testLength <= 25) {
+  imageOneEl.addEventListener('click', function (event) {
+    // allProducts[].clicks++;
+    console.log(event.target);
+    testLength++;
+    getCurrentDisplay(event);
+  });
+
 
 }
 
-getRandomProducts();
-
-// for (var numberOfProduct = 0; numberOfProduct < allProducts.length; numberOfProduct++) {
-//   new Product(productList[numberOfProduct]);
-// }
-
-
-// console.log(Product);
