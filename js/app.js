a61683a834b997f07bbfddb248e2521dd208757e
dlchambersjr <dlchambersjr@gmail.com>
2018-08-22 09:02:30 -0700

    Minor Tweaks

--- js/app.js
+++ js/app.js
@@ -115,6 +115,7 @@ function updateArraysForChart() {
   }
 }
 
+// Render the chart
 function drawResultsChart() {
   var ctx = document.getElementById('product-results-chart').getContext('2d');
   var resultsChart = new Chart(ctx, { // eslint-disable-line
